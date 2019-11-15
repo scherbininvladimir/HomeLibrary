b1124b8278b7e497e971cb2dbf1366393bd0d5d7
Vladimir <scherbinin.vladimir@gmail.com>
2019-11-15 10:22:58 +0300

    Unit D5.9 Формсеты

--- p_library/forms.py
+++ p_library/forms.py
@@ -1,7 +1,16 @@
 from django import forms  
-from p_library.models import Author  
+from django.forms import formset_factory
+
+from p_library.models import Author, Book
+
   
-class AuthorForm(forms.ModelForm):  
+class AuthorForm(forms.ModelForm):
+    full_name = forms.CharField(widget=forms.TextInput)   
     class Meta:  
         model = Author  
+        fields = '__all__'
+
+class BookForm(forms.ModelForm):  
+    class Meta:  
+        model = Book  
         fields = '__all__'
