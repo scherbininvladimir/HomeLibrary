c96e63cb2cec3c28534f0916de1aefaf43849f42
Vladimir <scherbinin.vladimir@gmail.com>
2019-12-07 23:06:23 +0300

    deploy

--- my_site/settings.py
+++ my_site/settings.py
@@ -13,7 +13,7 @@ from django.urls import reverse_lazy
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.environ.get('SECRET_KEY')
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,12 +79,7 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
