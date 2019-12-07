03dce6080b762dbf7e8e365c0c4a94b1be81f6ae
Vladimir <scherbinin.vladimir@gmail.com>
2019-12-07 23:23:02 +0300

    deploy

--- my_site/settings.py
+++ my_site/settings.py
@@ -14,19 +14,19 @@ import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.environ.get('SECRET_KEY')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(h2wp*&2#a7x2*=3v-+!1%0+qtjyi1z+$=l5cr+sb&8&=)jpi!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['frozen-everglades-83640.herokuapp.com']
 
 
 # Application definition
