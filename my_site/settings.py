378adca89999a97f52c6d6d717eb0a596569081c
Vladimir <scherbinin.vladimir@gmail.com>
2019-12-07 23:09:06 +0300

    deploy

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 from django.urls import reverse_lazy
+import dj_database_url 
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
